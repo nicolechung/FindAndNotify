d17c28f15ac3f7ca2c06a5a3460b6e520bb640f9
Nicole Chung <nchung@nascentdigital.com>
2015-11-28 13:38:24 -0500

    blank files

---
+++ FindAndNotify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindAndNotify
+//
+//  Created by Nicole Chung on 2015-11-28.
+//  Copyright © 2015 Nicole Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
